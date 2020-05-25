f8f4c2ff05fbfc58e1877b6cd0ac75bb1e51c590
namaeconde <namaeconde@gmail.com>
2020-05-25 21:29:39 +0800

    Refactor insertion sort

--- src/sort/InsertionSort.java
+++ src/sort/InsertionSort.java
@@ -2,8 +2,26 @@ package sort;
 
 public class InsertionSort {
 
+  public static int[] sort(int[] input, int[] sorted) {
+    // sort in ascending order
+    sorted[0] = input[0];
+    for(int i=1; i<input.length; i++) {
+      int j=i-1;
+      while(j>=0 && sorted[j] > input[i]) {
+        // move items in sorted array
+        sorted[j+1] = sorted[j];
+        j--;
+      }
+      // insert input to sorted array
+      sorted[j+1] = input[i];
+    }
+    return sorted;
+  }
+
   /**
-   * Sort array of integers using insertion sort algorithm.
+   * If asc = true
+   * sort array of integers using insertion sort algorithm in ascending order,
+   * otherwise in descending order.
    * O(n^2) time complexity.
    * @param input
    * @param asc
@@ -15,18 +33,7 @@ public class InsertionSort {
     if(input != null && input.length > 0) {
       sorted = new int[input.length];
       if (asc) {
-        // sort in ascending order
-        sorted[0] = input[0];
-        for(int i=1; i<input.length; i++) {
-          int j=i-1;
-          while(j>=0 && sorted[j] > input[i]) {
-            // move items in sorted array
-            sorted[j+1] = sorted[j];
-            j--;
-          }
-          // insert input to sorted array
-          sorted[j+1] = input[i];
-        }
+        return sort(input, sorted);
       } else {
         // sort in descending order
         sorted[0] = input[0];
